2f6d83b629306e5787d1ac09c623b63cfe67a824
yhotta240 <yhotta1224@gmail.com>
2024-04-30 00:07:07 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import youtubeData from './youtube_data.json';
 
 function App() {
 
+  const options = {
+    loop: 1,
+    mute: false,
+    inline: true,
+    autoPlay: true,
+    // width: 700,
+    // height: 400,
+  };
+
   return (
     <div>
       <div className='mb-12'>
@@ -14,8 +23,9 @@ function App() {
       <div className='m-4' >
         <YoutubeEmbed
           data={youtubeData}
+          options={options}
           maxWidth={600}
-          classContainer={"my-6 relative mx-auto"}
+          classContainer={"my-6 relative mx-auto "}
           classTitle={"md:text-xl mb-2 font-bold"}
           classIframe={"w-full aspect-video"}
         ></YoutubeEmbed>
--- src/components/youtubeEmbed.js
+++ src/components/youtubeEmbed.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const YoutubeEmbed = ({ data, maxWidth = 600, classContainer = "", classTitle = "", classIframe = "" }) => {
+// YoutubeEmbedコンポーネント
+// props:
+// - data: 動画情報の配列。各要素は{ title: string, url?: string, videoId?: string }形式であり、urlまたはvideoIdのいずれかを含む。
+// - options: 動画再生のオプションを指定するオブジェクト。オプションは{ loop?: number, mute?: boolean, inline?: boolean, autoplay?: boolean }の形式で、デフォルトは空のオブジェクト
+// - maxWidth: コンポーネントの最大幅（デフォルトは600px）
+// - classContainer: コンテナーのクラス名（デフォルトは空文字列）
+// - classTitle: タイトルのクラス名（デフォルトは空文字列）
+// - classIframe: iframeのクラス名（デフォルトは空文字列）
+
+const YoutubeEmbed = ({ data, options, maxWidth = 600, classContainer = "", classTitle = "", classIframe = "" }) => {
   // 動画情報が提供されない場合のデフォルト値
   const videoInfo = data || [];
 
@@ -8,15 +17,17 @@ const YoutubeEmbed = ({ data, maxWidth = 600, classContainer = "", classTitle = 
     <>
       {/* 動画情報のマップ */}
       {videoInfo.map((video, index) => (
-        <div key={index} className={`mb-4 ${classContainer}`} style={{ maxWidth: `${maxWidth}px` }}>
+        <div key={index} className={`${classContainer}`} style={{ maxWidth: `${maxWidth}px` }}>
           {/* タイトル */}
           <h2 className={`${classTitle}`}>{video.title}</h2>
           {/* 動画の埋め込み */}
           <iframe
+            width={`${options.width}`}
+            height={`${options.height}`}
             className={`${classIframe}`}
             title={video.title}
-            src={`https://www.youtube.com/embed/${getVideoId(video)}`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            src={`https://www.youtube.com/embed/${getVideoId(video)}?${Object.entries(options).map(([key, value]) => `${key}=${value}`).join('&')}`}
+            allow="accelerometer;  autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
             allowFullScreen
           ></iframe>
         </div>
